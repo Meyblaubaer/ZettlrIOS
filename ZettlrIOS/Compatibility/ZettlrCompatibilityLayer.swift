cae10a5279d1390173b67930567e59dbad8fc693
Meyblaubaer <sven-ch.meyhoefer@outlook.com>
2025-03-04 20:11:07 +0100

    Update Roadmap

---
+++ ZettlrIOS/Compatibility/ZettlrCompatibilityLayer.swift
@@ -0,0 +1,16 @@
+class ZettlrCompatibilityLayer {
+    // Dateiformat-Konvertierung
+    func convertToZettlrFormat(_ note: Note) -> Data {
+        // Konvertierung in Zettlr-kompatibles Format
+    }
+    
+    // Metadaten-Handling
+    func processZettlrMetadata(_ metadata: [String: Any]) {
+        // YAML Front Matter Verarbeitung
+    }
+    
+    // Link-System
+    func processZettlrLinks(_ content: String) -> String {
+        // Wiki-Link Konvertierung
+    }
+} 
